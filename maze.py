31f72b4bf56f3d0680db4b1961e43322ef2dc622
smitchler <sm249078@muhlenberg.edu>
2015-04-13 19:40:10 -0400

    test 9 passing

--- maze.py
+++ maze.py
@@ -1,5 +1,5 @@
 # Maze program by Sarah Mitchler
-setMediaPath('C:\\Users\\PC7\\Documents\\GitHub\\Maze')
+setMediaPath('C:\\Users\\Sarah\\Documents\\GitHub')
 class Maze(object):
   """ solves a maze """
   def __init__(self):
@@ -10,7 +10,36 @@ class Maze(object):
     self.w = makeWorld(w,h)
     self.w.setPicture(self.image)
     self.t = makeTurtle(self.w)
-
+    penUp(self.t)
+    moveTo(self.t, 30, 190)
+    turnLeft(self.t)
+   
+    
+  def colorInFront(self):
+    """ returns the color in front of the turtle """
+    x = getXPos(self.t)
+    y = getYPos(self.t)
+    dir = getHeading(self.t)
+    if dir == 0.0:
+      y = y-11
+    if dir == 90.0 or -270.0:
+      x = x+11
+    if dir == 180.0 or -180.0:
+      y = y+11
+    if dir == 270.0 or -90.0:
+      x = x-11
+    c = getColor(getPixelAt(self.image, x, y))
+    if distance(c, white) < 160:
+      return white
+    if distance(c, blue) < 160:
+      return blue
+    if distance(c, yellow) < 160:
+      return yellow
+    if distance(c, red) < 160:
+      return red
+    if distance(c, green) < 160:
+      return green 
+   
 #Tests follow this line
 doTests = true
 if doTests:
@@ -47,3 +76,30 @@ if doTests:
       printNow("Test 5 failed, it is not a turtle.")
   except:
     printNow("Test 5 failed, Turtle doesn't exist.")
+    
+  #Test 6: check Turtle's location
+  try:
+    if m.t.getXPos() == 30 and m.t.getYPos() == 190:
+      printNow("Test 6 passed, Turtle is in correct starting position.")
+    else:
+      printNow("Test 6 failed, Turtle is not in correct position.")
+  except:
+    printNow("Turtle does not exist.")
+    
+  #Test 7: check colorInFront
+  if dir(m).index('colorInFront') > 0:
+    printNow("Test 7 passed, colorInFront exists.")
+  else:
+    printNow("Test 7 failed, colorInFront does not exist.")
+    
+  #Test 8: check if colorInFront is white
+  if m.colorInFront() == white:
+    printNow("Test 8 passed, colorInFront returns white.")
+  else:
+    printNow("Test 8 failed, colorInFront does not return white.")
+    
+  #Test 9: check if colorInFront is blue
+  if m.colorInFront() == blue:
+    printNow("Test 9 passed, colorInFront returns blue.")
+  else:
+    printNow("Test 9 failed, colorInFront does not return blue.")
